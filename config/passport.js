3156562a883c68a8154d37f41dfb7f8fe8f0bdd9
kalyonnii <mudhiiguubbakalyonnii@gmail.com>
2025-03-26 18:21:13 +0530

    COMMIT

--- config/passport.js
+++ config/passport.js
@@ -2,10 +2,11 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
 
+
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
+  callbackURL: `${process.env.FRONTEND_URL}/auth/google/callback`
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ googleId: profile.id });
