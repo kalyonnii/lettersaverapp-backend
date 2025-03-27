1980315ce389efaf74bc4db36c7f34de199a1cb8
kalyonnii <mudhiiguubbakalyonnii@gmail.com>
2025-03-27 16:44:17 +0530

    commit

--- app.js
+++ app.js
@@ -195,20 +195,12 @@ const connectDB = async () => {
 connectDB();
 
 
-// Serve Angular App
-const angularPath = path.join(__dirname, 'dist', 'letter-saver-app');
-app.use(express.static(angularPath));
-
 // Routes
 app.use('/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/letters', letterRoutes);
 
 
-// Catch-All Route for Angular
-app.get('*', (req, res) => {
-  res.sendFile(path.join(angularPath, 'index.html'));
-});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
