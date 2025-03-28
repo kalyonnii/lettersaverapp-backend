222f8685ca7f058540b84ec52203d457d0ffb592
kalyonnii <mudhiiguubbakalyonnii@gmail.com>
2025-03-28 15:06:02 +0530

    COMMIT

--- app.js
+++ app.js
@@ -171,6 +171,9 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     secure: process.env.NODE_ENV === 'production', // Secure cookies in production
+    httpOnly: true,
+  sameSite: 'lax', // Use 'none' if cross-domain redirect is needed
+  maxAge: 24 * 60 * 60 * 1000
     // Allow cookies across origins
   }
 }));
