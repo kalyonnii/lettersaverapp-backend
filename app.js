191c31462e50517b315ff44311a179d7c74b8e1b
NARENDRA9959 <cnarendra329@gmail.com>
2025-04-03 00:19:48 +0530

    Done

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:4200',
+  origin: `${process.env.FRONTEND_URL}`,
   credentials: true
 }));
 app.use(express.json());
@@ -45,7 +45,7 @@ const User = mongoose.model('User', {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback" // Ensure it's correct!
+  callbackURL: `${process.env.BACKEND_URL}/auth/google/callback` // Ensure it's correct!
 },
   async (accessToken, refreshToken, profile, done) => {
     try {
@@ -106,7 +106,7 @@ app.get('/auth/google/callback',
   (req, res) => {
     // console.log(req)
     // Successful authentication, redirect home.
-    res.redirect('http://localhost:4200/editor');
+    res.redirect(`${process.env.FRONTEND_URL}/editor`);
   }
 );
 
